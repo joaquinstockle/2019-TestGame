f494aa1065487bd20c642006c0393cbe06b2f1a2
Joaquin Stockle <joaquinstockle@live.com>
2019-11-15 13:33:54 -0700

    Made new project for game and made character controller

---
+++ Fall 2019 Game/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float speed = 10f, jumpSpeed = 10f;
+
+    public float gravity = -5f;
+
+    private Vector3 velocity;
+
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity);
+
+        if (characterController.isGrounded)
+        { 
+            velocity.y = 0f;
+            
+            if (Input.GetButton("Jump"))
+            {
+                velocity.y = jumpSpeed * Time.deltaTime;
+            }
+            
+        }
+      
+            
+
+
+
+    }
+}
---
+++ Fall 2019 Game/Assets/Scripts/MouseControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+
+    public float mouseSpeed = 110f;
+
+    public Transform playerCam;
+
+    private float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerCam.Rotate(Vector3.up * mouseX);
+
+
+    }
+}
