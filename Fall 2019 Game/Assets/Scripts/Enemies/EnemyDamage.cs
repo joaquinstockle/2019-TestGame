2b9dbdf7c6cb9901bff8f73324ffc34167d84acf
Joaquin Stockle <joaquinstockle@live.com>
2019-11-17 18:19:35 -0700

    Completely redid health because I didnt like the other one

---
+++ Fall 2019 Game/Assets/Scripts/Enemies/EnemyDamage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float Amount;
+
+    public void OnTriggerStay(Collider other)
+    {
+        PlayerHealth.Addpoints(Amount);
+    }
+}
--- Fall 2019 Game/Assets/Scripts/Player/PlayerHealth.cs
+++ Fall 2019 Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,8 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public EnemyDamage damage;
     
+    public static float health;
+
+    public Image healthBar;
+
+    public bool Dead = false;
+
+    
+
+
+
+    private void Start()
+    {
+        health = 1f;
+    }
+    
+    public static void Addpoints (float Amount) 
+    {
+        health += Amount * Time.deltaTime;
+    }
+
+    
+    void Update()
+    {
+        healthBar.fillAmount = health;
+
+        
+        if (health < 0)
+            health = 0;
+        if (health > 1)
+            health = 1;
+
+        if (health <= 0)
+            Dead = true;
+        if (health > 0)
+            Dead = false;
+
+        if (Dead)
+        {
+
+        }
+    }
 }
