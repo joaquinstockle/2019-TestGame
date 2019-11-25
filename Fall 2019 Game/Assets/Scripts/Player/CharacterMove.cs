661b13c749462c36ad1437aa86c87f2ee4e2f937
Joaquin Stockle <joaquinstockle@live.com>
2019-11-25 02:49:12 -0700

    Added sprint mechanic

--- Fall 2019 Game/Assets/Scripts/Player/CharacterMove.cs
+++ Fall 2019 Game/Assets/Scripts/Player/CharacterMove.cs
@@ -2,27 +2,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class CharacterMove : MonoBehaviour
 {
     public CharacterController characterController;
 
-    public float speed = 10f, jumpSpeed = 10f;
-
+    public float walk = 10f, jumpSpeed = 10f, sprint = 16f;
+    public float speeds;
     public float gravity = -1f;
-
     private Vector3 velocity;
-    
 
+    public Image staminaBar;
+    public static float stamina;
+    public bool fatigue;
+    public float stamRes;
+    public bool full;
+    public GameObject fullBar;
+
+
+    private void Start()
+    {
+        stamina = 1f;
+        fatigue = false;
+    }
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift) && !fatigue)
+        {
+            speeds = sprint;
+            stamRes = -1;
+            fullBar.SetActive(true);
+
+        }
+        else
+        {
+            speeds = walk;
+            fullBar.SetActive(true);
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        characterController.Move(move * speed * Time.deltaTime);
+        characterController.Move(move * speeds * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
@@ -38,9 +64,34 @@ public class CharacterMove : MonoBehaviour
             }
             
         }
-      
-            
 
+        staminaBar.fillAmount = stamina;
+        stamina += stamRes * Time.deltaTime;
+
+        if (!fatigue)
+            stamRes = .3f;
+        if (fatigue)
+            stamRes = .15f;
+        
+        if (stamina < 0)
+            stamina = 0;
+        if (stamina > 1)
+            stamina = 1;
+        
+        if (stamina <= 0)
+            fatigue = true;
+        if (stamina >= 1)
+        {
+            fatigue = false;
+            full = true;
+        }
+
+        if (stamina < 1)
+            full = false;
+        
+        if (full)
+            fullBar.SetActive(false);
+            
 
 
     }
